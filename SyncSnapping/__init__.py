552cefcf14556356043276f2a856c439f9f926b4
ssnd292 <3914410+ssnd292@users.noreply.github.com>
2024-03-14 11:46:41 +0100

    ! faulty Setup

--- SyncSnapping/__init__.py
+++ SyncSnapping/__init__.py
@@ -1,8 +1,22 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# INFO about the addon in USERPREFS
 bl_info = {
     "name" : "SyncSnapping",
     "author" : "Sebastian Schneider",
-    "description" : "Syncs enabling and disabling snapping in both 3D View and UV Window",
-    "blender" : (3, 6, 2),
+    "description" : "Syncs enabling and disabling snapping in both 3D View and UV Window. Shortcut on S. Can be changed at any time in the Keymap menu.",
+    "blender" : (3, 6, 5),
     'version': (0, 1, 0 ,0),
     "location" : "View3D",
     "warning" : "",
@@ -11,9 +25,6 @@ bl_info = {
 
 import bpy
 from bpy.utils import register_class, unregister_class
-
-from bpy.types import AddonPreferences
-from bpy.props import (PointerProperty)
 
 class SNAP_OT_SwitchSnapping(bpy.types.Operator):
     bl_idname = "snap.switchsnapping"
